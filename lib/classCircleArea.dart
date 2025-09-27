251797332750d9b7311fac87f954d0171f116eb9
BinoKim <euibin9738@gmail.com>
2025-09-27 16:51:36 +0900

    주석달기

--- lib/classCircleArea.dart
+++ lib/classCircleArea.dart
@@ -1,4 +1,5 @@
 
+// 원의 면적을 구하는 클래스 생성
 class Circle {
   double radius;
   String name;
@@ -12,6 +13,9 @@ class Circle {
 
 void main() {
   
+  // Circle 클래스를 이용해 newArea라는 객체 생성
   Circle newArea = Circle(5, "First");
+
+  // Circle 클래스에 있는 메서드를 활용하는 문구를 출력
   print("${newArea.name} 원의 넓이는 ${newArea.getArea()} 입니다.");
 }
